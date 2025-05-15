36e7feb0d656af281d79787e44e8ab22f080ca76
Kevin Macdonald <kjm25@cecs.pdx.edu>
2025-05-15 15:23:41 -0700

    not working, but progress

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ pub const RED: u8 = 0;
 pub const GREEN: u8 = 1;
 pub const BLUE: u8 = 2;
 
+use microbit::{board, pac};
 // use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};                                   
 use panic_rtt_target as _;                                                    
 
-use embedded_hal::{delay::DelayNs, digital::OutputPin};
-use embedded_hal::digital::{ PinState, InputPin };
+use embedded_hal::{delay::DelayNs, digital::OutputPin, digital::InputPin};
+use embedded_hal::digital::{ PinState };
+use embedded_hal_bus::spi::{ExclusiveDevice, NoDelay};
 
 use bme280::spi::BME280;
 
@@ -20,8 +22,9 @@ use bme280::spi::BME280;
 use microbit::{
     board::Board,
     gpio::{MOSI, MISO, SCK},
-    hal::gpio::{ Pin, Level, DriveConfig, OpenDrainConfig, Input, Output, PushPull, Floating, p0::P0_01, p0::P0_13, p0::P0_17},
+    hal::gpio::{ Pin, Level, DriveConfig, OpenDrainConfig, Input, Output, PushPull, Floating, p0::P0_01, p0::P0_13, p0::P0_14, p0::P0_15},
     hal::timer::Timer,
+    hal::Delay,
     hal::uarte::{Uarte, Baudrate, Parity},
     hal::spi::{Spi, Pins, Frequency},
     hal::spim::{MODE_0, Polarity, Phase, Mode},
@@ -45,17 +48,26 @@ fn start_here() -> ! {
     rtt_init_print!();
     let mut board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let mut spi_timer = Timer::new(board.TIMER1);
+    let mut bme_timer = Timer::new(board.TIMER2);
 
     let mut cs_pin = board.edge.e16.into_push_pull_output(Level::High);
-    // let mi: P0_13<Input<Floating>>;
-    // let s: P0_17<Output<PushPull>>;
-    // let mut miso   = mi.into_floating_input();
-    // let mut sck    = s.into_push_pull_output(Level::Low);
+    //let mi: P0_13<Input<Floating>>;
+    //let peripherals = pac::Peripherals::take().unwrap();
+    //let mut p0_parts = microbit::hal::gpio::p0::Parts::new(peripherals.P0);
+
+    let mut sck =  board.pins.p0_17.into_push_pull_output(Level::Low);
+    let mut miso  =  board.pins.p0_01.into_floating_input();
+    let mut mosi =  board.pins.p0_13.into_push_pull_output(Level::Low);
+    
+    // let coretx_peripherals = cortex_m::Peripherals::take().unwrap();
+    // let mut delay = Delay::new(coretx_peripherals.SYST);
+
 
     let p = Pins {
-        sck:  core::prelude::v1::Some(Pin::into_push_pull_output(Level::Low)),
-        mosi: core::prelude::v1::Some(Pin::into_push_pull_output(Level::Low)),
-        miso: core::prelude::v1::Some(Pin::into_floating_input()),
+        sck:  core::prelude::v1::Some(sck.degrade()),
+        mosi: core::prelude::v1::Some(mosi.degrade()),
+        miso: core::prelude::v1::Some(miso.degrade()),
     };
     
     let s = Spi::new(
@@ -64,6 +76,8 @@ fn start_here() -> ! {
         Frequency::K250, 
         Mode{ polarity: Polarity::IdleLow, phase: Phase::CaptureOnFirstTransition },  
     );
+
+    let spi_dev = ExclusiveDevice::new(board.SPI0, cs_pin, spi_timer);
 
     // Set up UARTE for microbit v2 using UartePort wrapper
     let mut serial = Uarte::new(
@@ -75,47 +89,19 @@ fn start_here() -> ! {
 
     // Clear terminal
     serial_write(&mut serial, b"\x1Bc");
-    
-    // Setup the low side of the matrix LED
-    board.display_pins.col5.set_state(PinState::Low).unwrap();
-    
-    // Setup the RGB LED pins as open drain with inital setting LOW
-    // Turns off the RGB LED
-    let mut red_left = board.edge.e00.into_open_drain_output(OpenDrainConfig::HighDrive0Disconnect1,Level::Low);
-    let mut green_left = board.edge.e01.into_open_drain_output(OpenDrainConfig::HighDrive0Disconnect1,Level::Low);
-    let mut blue_left = board.edge.e02.into_open_drain_output(OpenDrainConfig::HighDrive0Disconnect1,Level::Low);
 
-    // Initial color state
-    let mut color_state = RED;
 
+    let mut bme280 = BME280::new(spi_dev.unwrap()).unwrap();
+    bme280.init(&mut bme_timer).unwrap();
+    
+    
     loop {
+        // measure temperature, pressure, and humidity
+        let measurements= bme280.measure(&mut bme_timer).unwrap();
 
-        // Toggle the Matrix LED on
-        board.display_pins.row3.set_state(PinState::High).unwrap();
-        
-        // depending on color state, turn on the RGB LED
-        if color_state == RED   {red_left.set_high().unwrap(); serial_write(&mut serial, b"Red\n\r"); }
-        if color_state == GREEN {green_left.set_high().unwrap(); serial_write(&mut serial, b"Green\n\r"); }
-        if color_state == BLUE  {blue_left.set_high().unwrap(); serial_write(&mut serial, b"Blue\n\r"); }
-
-        // On time
-        timer.delay_ms(500);
-
-        // Toggle the Matrix LED off
-        board.display_pins.row3.set_state(PinState::Low).unwrap();
-
-        // depending on color state, turn off the RGB LED
-        if color_state == RED   {red_left.set_low().unwrap();}
-        if color_state == GREEN {green_left.set_low().unwrap();}
-        if color_state == BLUE  {blue_left.set_low().unwrap();}
-
-        // Change color state to switch between RGB LEDs
-        if color_state > 1 {
-            color_state = RED;
-            serial_write(&mut serial, b"\x1Bc");
-        } else {
-            color_state += 1;
-        }
+        rprintln!("Relative Humidity = {}%", measurements.humidity);
+        rprintln!("Temperature = {} deg C", measurements.temperature);
+        rprintln!("Pressure = {} pascals", measurements.pressure);
 
         // Off time
         timer.delay_ms(500);
